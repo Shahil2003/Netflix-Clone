2278d0f598fa779e65302293267a8642c79a346d
Shahil2003 <muhammedshahil69@gmail.com>
2024-01-06 10:26:58 +0530

    new commit

---
+++ src/urls/urls.jsx
@@ -0,0 +1,4 @@
+import {API_KEY} from '../constants/constants'
+import {baseUrl} from '../constants/constants'
+export const originals = `${baseUrl}/discover/tv?api_key=${API_KEY}&with_networks=213`
+export const action = `${baseUrl}/discover/movie?api_key=${API_KEY}&with_genres=28`
